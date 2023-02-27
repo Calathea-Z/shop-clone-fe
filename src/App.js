913af635477abfbcb7377072174c46031c4ef9a9
Calathea-Z <zsykes21@gmail.com>
2023-02-27 16:24:14 -0500

    Added product page.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import data from "./data";
 
 function App() {
   return (
@@ -6,7 +7,25 @@ function App() {
         <a href='/'>Post Shop</a>
       </header>
       <main>
-        list products
+        <h1>Featured Products </h1>
+        <div className='products'>
+        {
+          data.products.map((product, index) => (
+            <div className='product' key={index}>
+              <a href={`product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className='product-info'>
+              <a href={`product/${product.slug}`}>
+                <p> {product.name} </p>
+              </a>
+              <p> <strong> ${product.price} </strong> </p>
+              <button>Add to cart</button>
+              </div>
+            </div>
+          ))
+        }
+        </div>
       </main>
     </div>
   );
---
+++ src/data.js
@@ -0,0 +1,42 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            slug: 'nike-slim-shirt',
+            category: 'Shirts',
+            image: '/assets/shirt.jpeg',
+            price: 120, 
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image: '/assets/avllogo.png',
+            price: 120, 
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality pants',
+        },
+        {
+            name: 'Adidas Slim Shirt',
+            slug: 'adidas-slim-shirt',
+            category: 'Shirts',
+            image: '/assets/shirt.jpeg',
+            price: 120, 
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.2,
+            numReviews: 10,
+            description: 'high quality shirt',
+        },
+    ]
+}
+
+export default data
