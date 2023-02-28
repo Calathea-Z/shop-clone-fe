2c4b39f1729712aae0c6ccfda6d130dd7d077879
Calathea-Z <zsykes21@gmail.com>
2023-02-28 12:30:34 -0500

    Found issue with cart bug.

--- src/Pages/Product.js
+++ src/Pages/Product.js
@@ -54,14 +54,17 @@ const Product = () => {
   const { cart } = state;
 
   const addToCartHandler = async () => {
-    const existItem = cart.cartItems.find((i) => i.id === product._id);
+    const existItem = cart.cartItems.find((x) => x._id === product._id);
     const quantity = existItem ? existItem.quantity + 1 : 1;
     const { data } = await axios.get(`/api/products/${product._id}`);
     if (data.countInStock < quantity) {
       window.alert(`Sorry, ${product.name} is out of stock`);
       return
     }
-    cartDispatch({type:'CART_ADD_ITEM', payload: {...product, quantity} })
+    cartDispatch(
+      {
+      type:'CART_ADD_ITEM', 
+      payload: {...product, quantity} })
   };
 
   return loading ? (
