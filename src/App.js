3c3c011eb60c39eccfe66edacdfd1f13aaacc4e2
Calathea-Z <zsykes21@gmail.com>
2023-02-27 19:07:50 -0500

    Added Product Card and Rating componenet. Added Bootstrap styling.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,40 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Home from "./Pages/Home";
-import Product from "./Pages/Product";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { LinkContainer } from "react-router-bootstrap";
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <Link to='/'>Post Shop</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path='/product/:slug' element={ <Product />} />
-          <Route path='/'element= { <Home /> } />
-        </Routes>  
-      </main>
-    </div>
+      <div className='d-flex flex-column site-container'>
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Post Shop !</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<Product />} />
+              <Route path="/" element={<Home />} />
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className='text-center'>
+            <h6>
+              All Rights Reserved
+            </h6>
+          </div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
