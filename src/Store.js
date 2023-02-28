787b756a83ade78398b41d3a4a458e2dcaaf4115
Calathea-Z <zsykes21@gmail.com>
2023-02-28 09:52:50 -0500

    Begin adding global context. Have working cart item count.

--- src/Store.js
+++ src/Store.js
@@ -26,6 +26,6 @@ function reducer(state, action) {
 
 export function StoreProvider(props) {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const value = [state, dispatch];
-  return <Store.Provider value={value}>{props.children}</Store.Provider>;
+  const value = {state, dispatch};
+  return <Store.Provider value={value}> {props.children} </Store.Provider>;
 }
