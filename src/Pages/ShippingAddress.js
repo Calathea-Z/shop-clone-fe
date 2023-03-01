43cd5cdaef235482ab8fe8788711075a91db4523
Calathea-Z <zsykes21@gmail.com>
2023-03-01 15:30:58 -0500

    Add checkoutSteps bar to top of checkout page. Add logic to clear user address info and cart on logout. Add logic to reroute user to signin but save cart if they arent signed in and have begun shopping.

--- src/Pages/ShippingAddress.js
+++ src/Pages/ShippingAddress.js
@@ -1,81 +1,129 @@
 import { Helmet } from "react-helmet-async";
 import Form from "react-bootstrap/Form";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Button from "react-bootstrap/esm/Button";
+import { useNavigate } from "react-router-dom";
+import { Store } from "../Store";
+import CheckoutSteps from "../components/CheckoutSteps";
+
 
 export default function ShippingAddress() {
-  const [fullName, setFullName] = useState("");
-  const [address, setAddress] = useState("");
-  const [city, setCity] = useState("");
-  const [zipCode, setZipCode] = useState("");
-  const [country, setCountry] = useState("");
-  const [state, setState] = useState("");
+  const navigate = useNavigate();
+
+  const { state, dispatch: contextDispatch } = useContext(Store);
+  const {
+    userInfo,
+    cart: { shippingAddress },
+  } = state;
+
+  const [fullName, setFullName] = useState(shippingAddress.fullName || "");
+  const [address, setAddress] = useState(shippingAddress.address || "");
+  const [city, setCity] = useState(shippingAddress.city || "");
+  const [zipCode, setZipCode] = useState(shippingAddress.zipCode || "");
+  const [country, setCountry] = useState(shippingAddress.country || "");
+  const [stateName, setStateName] = useState(shippingAddress.stateName || "");
 
   const submitHandler = (e) => {
     e.preventDefault();
-  };
 
+    contextDispatch({
+      type: 'SAVE_SHIPPING_ADDRESS',
+      payload: {
+        fullName,
+        address,
+        city,
+        zipCode,
+        country,
+        stateName,
+      },
+    });
+
+    localStorage.setItem(
+      'shippingAddress',
+      JSON.stringify({
+        fullName,
+        address,
+        city,
+        zipCode,
+        country,
+        stateName,
+      }),
+      );
+
+      navigate('/payment');
+  };
+  
+  useEffect(() => {
+    if (!userInfo) {
+      navigate('/signin?redirect=/shipping')
+    }
+
+  },[userInfo, navigate])
   return (
     <div>
       <Helmet>
         <title>Shipping Address</title>
       </Helmet>
-      <h1 className="my-3">Shipping Address</h1>
-      <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="fullName">
-          <Form.Label>Full Name</Form.Label>
-          <Form.Control
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="address">
-          <Form.Label>Address</Form.Label>
-          <Form.Control
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="city">
-          <Form.Label>City</Form.Label>
-          <Form.Control
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="state">
-          <Form.Label>State</Form.Label>
-          <Form.Control
-            value={state}
-            onChange={(e) => setState(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="zipCode">
-          <Form.Label>Zip Code</Form.Label>
-          <Form.Control
-            value={zipCode}
-            onChange={(e) => setZipCode(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="country">
-          <Form.Label>Country</Form.Label>
-          <Form.Control
-            value={country}
-            onChange={(e) => setCountry(e.target.value)}
-            required
-          />
-        </Form.Group>
-        <div className="mb-3">
-          <Button variant="primary" type="submit">
-            Continue
-          </Button>
-        </div>
-      </Form>
+
+      <CheckoutSteps step1 step2 />
+      <div className="container small-container">
+        <h1 className="my-3">Shipping Address</h1>
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="fullName">
+            <Form.Label>Full Name</Form.Label>
+            <Form.Control
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="address">
+            <Form.Label>Address</Form.Label>
+            <Form.Control
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="city">
+            <Form.Label>City</Form.Label>
+            <Form.Control
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="stateName">
+            <Form.Label>State</Form.Label>
+            <Form.Control
+              value={stateName}
+              onChange={(e) => setStateName(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="zipCode">
+            <Form.Label>Zip Code</Form.Label>
+            <Form.Control
+              value={zipCode}
+              onChange={(e) => setZipCode(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="country">
+            <Form.Label>Country</Form.Label>
+            <Form.Control
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+              required
+            />
+          </Form.Group>
+          <div className="mb-3">
+            <Button variant="primary" type="submit">
+              Continue
+            </Button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 }
