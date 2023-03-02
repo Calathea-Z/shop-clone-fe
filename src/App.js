ba15dacff27c589c3c8d1a62876a4203f7f8af4c
Calathea-Z <zsykes21@gmail.com>
2023-03-02 16:51:26 -0500

    Add in mobile responsive hamburger menu.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css'
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import { LinkContainer } from "react-router-bootstrap";
 import Badge from "react-bootstrap/esm/Badge";
-import Nav from 'react-bootstrap/Nav';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
 import { useContext } from "react";
 import { Store } from "./Store";
 import Cart from "./pages/Cart";
@@ -25,78 +25,81 @@ function App() {
   const { cart, userInfo } = state;
 
   const signoutHandler = () => {
-    contextDispatch({ type: 'USER_SIGNOUT' });
-    localStorage.removeItem('userInfo');
-    localStorage.removeItem('shippingAddress')
-    localStorage.removeItem('paymentMethod')
-  }
+    contextDispatch({ type: "USER_SIGNOUT" });
+    localStorage.removeItem("userInfo");
+    localStorage.removeItem("shippingAddress");
+    localStorage.removeItem("paymentMethod");
+  };
 
   return (
     <BrowserRouter>
-      <div className='d-flex flex-column site-container'>
-        <ToastContainer position='bottom-center' limit={1} />
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
         <header>
-          <Navbar bg="dark" variant="dark">
+          <Navbar bg="dark" variant="dark" expand="lg">
             <Container>
               <LinkContainer to="/">
                 <Navbar.Brand>Z-Shop</Navbar.Brand>
               </LinkContainer>
-              <Nav className='me-auto'>
-                <Link to='/cart' className='nav-link'>
-                  Cart{" "}
-                  {cart.cartItems.length > 0 && (
-                    <Badge pill bg='danger'>
-                      {cart.cartItems.reduce((a,c) => a + c.quantity, 0)}
-                    </Badge>
-                  )}
-                </Link>
-                {userInfo ? (
-                  <NavDropdown title={userInfo.name} id='basic-nav-dropdown'>
-                    <LinkContainer to='/profile'>
-                      <NavDropdown.Item>User Profile</NavDropdown.Item>
-                    </LinkContainer>
-                    <LinkContainer to='/orderhistory'>
-                      <NavDropdown.Item>Order History</NavDropdown.Item>
-                    </LinkContainer>
-                    <NavDropdown.Divider />
-                    <Link
-                      className='dropdown-item'
-                      to='#signout'
-                      onClick={signoutHandler}
-                    >
-                      Sign Out
-                    </Link>
-                  </NavDropdown>
-                ) : (
-                  <Link className='nav-link' to='/signin'>
-                  Sign In 
+              <Navbar.Toggle aria-controls="basic-navbar-nav" />
+              <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto w-100 justify-content-end">
+                  <Link to="/cart" className="nav-link">
+                    Cart{" "}
+                    {cart.cartItems.length > 0 && (
+                      <Badge pill bg="danger">
+                        {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                      </Badge>
+                    )}
                   </Link>
-                )}
-              </Nav>
+                  {userInfo ? (
+                    <NavDropdown title={userInfo.name} id="basic-nav-dropdown">
+                      <LinkContainer to="/profile">
+                        <NavDropdown.Item>User Profile</NavDropdown.Item>
+                      </LinkContainer>
+                      <LinkContainer to="/orderhistory">
+                        <NavDropdown.Item>Order History</NavDropdown.Item>
+                      </LinkContainer>
+                      <NavDropdown.Divider />
+                      <Link
+                        className="dropdown-item"
+                        to="#signout"
+                        onClick={signoutHandler}
+                      >
+                        Sign Out
+                      </Link>
+                    </NavDropdown>
+                  ) : (
+                    <Link className="nav-link" to="/signin">
+                      Sign In
+                    </Link>
+                  )}
+                </Nav>
+              </Navbar.Collapse>
             </Container>
           </Navbar>
         </header>
 
         <main>
-          <Container className='mt-3'>
+          <Container className="mt-3">
             <Routes>
-              <Route path="/product/:slug" element={<Product /> } />
-              <Route path="/" element={<Home /> } />
-              <Route path='/cart' element={<Cart /> } />
-              <Route path='/signin' element={<SignIn /> } />
-              <Route path='/shipping' element={<ShippingAddress /> } />
-              <Route path='/signup' element={<SignUp /> } />
-              <Route path='/payment' element={<Payment /> } />
-              <Route path='/previeworder' element={<PreviewOrder /> } />
-              <Route path='/order/:id' element={<OrderFinal /> } />
-              <Route path='/orderhistory' element={<OrderHistory /> } />
+              <Route path="/product/:slug" element={<Product />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/cart" element={<Cart />} />
+              <Route path="/signin" element={<SignIn />} />
+              <Route path="/shipping" element={<ShippingAddress />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/payment" element={<Payment />} />
+              <Route path="/previeworder" element={<PreviewOrder />} />
+              <Route path="/order/:id" element={<OrderFinal />} />
+              <Route path="/orderhistory" element={<OrderHistory />} />
             </Routes>
           </Container>
         </main>
         <footer>
-          <div className='text-center'>
+          <div className="text-center">
             <h6>
-              Check out my <a href='https:/zach-sykes.com'>Portfolio</a>
+              Check out my <a href="https:/zach-sykes.com">Portfolio</a>
             </h6>
           </div>
         </footer>
